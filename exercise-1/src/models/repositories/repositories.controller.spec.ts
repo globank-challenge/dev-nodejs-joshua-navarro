a995e617076d70cff1b636b11757f5a1663d8906
Joshua Navarro <joshua.navarro@globant.com>
2022-07-22 11:39:17 -0500

    add unit test for repositories controller [exercise-1]

--- exercise-1/src/models/repositories/repositories.controller.spec.ts
+++ exercise-1/src/models/repositories/repositories.controller.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RepositoriesController } from './repositories.controller';
+import { RepositoriesService } from './repositories.service';
 
 describe('RepositoriesController', () => {
   let controller: RepositoriesController;
+  let mockRepositoriesService: Partial<RepositoriesService>;
 
   beforeEach(async () => {
+    mockRepositoriesService = {
+      find: () => Promise.resolve([{ id: 1, state: 604 }]),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RepositoriesController],
+      providers: [
+        {
+          provide: RepositoriesService,
+          useValue: mockRepositoriesService,
+        },
+      ],
     }).compile();
 
     controller = module.get<RepositoriesController>(RepositoriesController);
@@ -14,5 +26,15 @@ describe('RepositoriesController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('find function', () => {
+    it('return an object with the list of repositories', async () => {
+      const response = await controller.find();
+
+      expect(typeof response).toBe('object');
+      expect(response).toHaveProperty('repositories');
+      expect(Array.isArray(response.repositories)).toBe(true);
+    });
   });
 });
