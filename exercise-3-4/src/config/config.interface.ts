bc5b94ee14439e619323b6c4a2a9899af7dc38af
Joshua Navarro <joshua.navarro@globant.com>
2022-07-25 17:47:03 -0500

    update config to add url base to exercise-1 server

--- exercise-3-4/src/config/config.interface.ts
+++ exercise-3-4/src/config/config.interface.ts
@@ -1,6 +1,11 @@
 export interface IConfig {
   env: string;
   database: IDatabaseConfig;
+  server: IServerConfig;
+}
+
+export interface IServerConfig {
+  repositoryUri: string;
 }
 
 export interface IDatabaseConfig {
--- exercise-3-4/src/config/configuration.ts
+++ exercise-3-4/src/config/configuration.ts
@@ -2,6 +2,9 @@ import { IConfig } from './config.interface';
 
 export default (): IConfig => ({
   env: process.env.NODE_ENV,
+  server: {
+    repositoryUri: process.env.SERVER_URI_REPOSITORY,
+  },
   database: {
     url: process.env.DATABASE_URL,
     cluster: process.env.DATABASE_CLUSTER,
